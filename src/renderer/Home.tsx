f4e214249dc6671dcc4dead52d014f7c1fef262e
alex-hobby <alexhobby98@gmail.com>
2022-09-07 15:01:56 -0400

    Render procedures based on db

--- src/renderer/Home.tsx
+++ src/renderer/Home.tsx
@@ -1,9 +1,23 @@
+import Procedures from 'db/Procedures';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ProcedureDropdown from './ProcedureDropdown';
 
 const Home = () => {
-  const procedures = ['test1', 'test2', 'alexiscool'];
-  return <ProcedureDropdown procedures={procedures} />;
+  const proceduresDefault = ['test1', 'test2', 'alexiscool'];
+  const [procedures, setProcedures] = useState<any[]>();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const procs = new Procedures();
+      const newProcs = await procs.getProceduresForDB(['React']);
+      setProcedures(newProcs.get('React'));
+    };
+
+    fetch();
+  }, []);
+
+  return <ProcedureDropdown procedures={procedures || proceduresDefault} />;
 };
 
 export default Home;
