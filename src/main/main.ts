5d00388d828204bbd4587754ce4cb62d07b24946
Aditya Vidyadharan <aditya@vidyadharan.com>
2022-09-09 00:55:44 -0400

    Implement IPC renderer for procedures

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+import Procedures from '../db/Procedures';
 
 class AppUpdater {
   constructor() {
@@ -29,6 +30,14 @@ ipcMain.on('ipc-example', async (event, arg) => {
   const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
   console.log(msgTemplate(arg));
   event.reply('ipc-example', msgTemplate('pong'));
+});
+
+ipcMain.handle('procedures:listProcedures', () => {
+  return new Procedures().getProceduresForDB(['React']);
+});
+
+ipcMain.handle('procedures:listDatabases', () => {
+  return new Procedures().getDatabases();
 });
 
 if (process.env.NODE_ENV === 'production') {
