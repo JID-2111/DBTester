4edcec85efa3df4282d6d365debdbc70efb8639e
Jack <jackfratto@gmail.com>
2022-09-21 10:53:45 -0400

    Update src/renderer/preload.d.ts
    Co-authored-by: Ishaan Guha <39541123+insaan21@users.noreply.github.com>

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -25,7 +25,7 @@ declare global {
         fetch(): Promise<ConnectionModel[]>;
         create(model: ConnectionModel): Promise<ConnectionModel>;
         select(id: number): null;
-        delete(id: number): Promise<null>;
+        delete(id: number): Promise<void>;
       };
     };
   }
