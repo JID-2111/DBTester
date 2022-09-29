be4ff13a81f57dea02a2d4348d6b1eedcbb5f56e
smulloy3 <smulloy3@gatech.edu>
2022-09-29 00:27:52 -0400

    Modified ui layout

--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -8,35 +8,35 @@ import '../scss/Home.scss';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to DB Tester</h1>
       <div className="menu-wrapper">
         <div className="recent-wrapper">
           <RecentList />
         </div>
         <div className="launch-wrapper">
+          <h1>Welcome to DB Tester</h1>
           <Link to="/">
-            <Button variant="secondary" className="launch-button">
+            <Button variant="link" className="launch-button">
               <Plus className="icon" height="2em" width="2em" />
               <h2>New Connection</h2>
             </Button>
           </Link>
           <Link to="/">
-            <Button variant="secondary" className="launch-button">
+            <Button variant="link" className="launch-button">
               <Link45deg className="icon" height="2em" width="2em" />
               <h2>Connect to Existing</h2>
             </Button>
           </Link>
           <Link to="/">
-            <Button variant="secondary" className="launch-button">
+            <Button variant="link" className="launch-button">
               <Archive className="icon" height="2em" width="2em" />
               <h2>View History</h2>
             </Button>
           </Link>
         </div>
+        <Link to="/execute">
+          <Button className="execute-button">Execute</Button>
+        </Link>
       </div>
-      <Link to="/execute">
-        <Button className="execute-button">Execute</Button>
-      </Link>
     </div>
   );
 };
