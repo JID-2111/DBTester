e91a4bc66b7cc0da092eaa6c6e97f7786690cc64
alex-hobby <alexhobby98@gmail.com>
2022-09-30 16:37:28 -0400

    add basic functionality for switching dbs

---
+++ src/renderer/components/DBDropdown.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+import '../scss/App.scss';
+
+interface IDBDropdownProps {
+  activeDb: string;
+  updateDb: (database: string) => void;
+}
+
+const DBDropdown = ({ activeDb, updateDb }: IDBDropdownProps) => {
+  const [databases, setDatabases] = useState<string[]>(['']);
+
+  useEffect(() => {
+    const fetchDBs = async () => {
+      const dbs = await window.procedures.ipcRenderer.fetchDatabases();
+      setDatabases(dbs);
+    };
+    fetchDBs();
+  }, []);
+
+  const handleClick = async (database: string) => {
+    updateDb(database);
+    await window.connections.ipcRenderer.switch(database);
+  };
+
+  return (
+    <Dropdown className="dropdown">
+      <Dropdown.Toggle variant="primary">{activeDb}</Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {databases.map((database: string) => {
+          const dbKey = `database- + ${database}`;
+          return (
+            <Dropdown.Item key={dbKey} onClick={() => handleClick(database)}>
+              {database}
+            </Dropdown.Item>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default DBDropdown;
--- src/renderer/components/Execute.tsx
+++ src/renderer/components/Execute.tsx
@@ -5,13 +5,35 @@ import { Link } from 'react-router-dom';
 import ProcedureDropdown from './ProcedureDropdown';
 
 import '../scss/Execute.scss';
+import DBDropdown from './DBDropdown';
 
 const Execute = () => {
   const [code, setCode] = useState<string>('');
+  const [activeDb, setActiveDb] = useState<string>('React');
+  const [activeProcedure, setActiveProcedure] = useState<string>('');
+
+  const updateDb = (database: string) => {
+    setActiveDb(database);
+    setActiveProcedure('');
+    setCode('');
+  };
+
   return (
     <div>
       <h1>Execute Stored Procedures</h1>
-      <ProcedureDropdown setCode={setCode} />
+      <div>
+        <h6>Selected Database</h6>
+        <DBDropdown activeDb={activeDb} updateDb={updateDb} />
+      </div>
+      <div>
+        <h6>Selected Procedure</h6>
+        <ProcedureDropdown
+          activeDb={activeDb}
+          activeProcedure={activeProcedure}
+          setActiveProcedure={setActiveProcedure}
+          setCode={setCode}
+        />
+      </div>
       <div className="execute-wrapper">
         <p className="procedure-code">{code}</p>
       </div>
