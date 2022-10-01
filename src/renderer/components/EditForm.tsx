62df751fd6d268d933f59898a8a3dac2c93be255
smulloy3 <smulloy3@gatech.edu>
2022-10-01 00:39:34 -0400

    get db to edit

--- src/renderer/components/EditForm.tsx
+++ src/renderer/components/EditForm.tsx
@@ -1,21 +1,60 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { ConnectionModel, ConnectionModelType } from '../../db/Models';
+import DBProvider from '../../db/entity/enum';
+import { ConnectionModel } from '../../db/Models';
 import '../scss/RecentConnections.scss';
 
-const EditForm = (db: ConnectionModel) => {
-  const [value, setValue] = useState<ConnectionModel>(db);
-  useEffect(() => {
-    console.log(db);
-  });
+interface INewConnectionForm {
+  id: number;
+  nickname: string;
+  type: DBProvider.PostgreSQL;
+  connectionConfig: {
+    config: 'manual';
+    address: string;
+    port: number;
+    username: string;
+    password: string;
+  };
+  createdDate: undefined;
+  lastUsed: undefined;
+}
+
+const EditForm = () => {
+  const [form, setForm] = useState<ConnectionModel>(); // Todo: fix this
+  const updateField = (field: string, value: string) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+  };
+  const saveConfig = async (event: React.FormEvent<HTMLElement>) => {
+    event.preventDefault();
+  };
+  useEffect(() => {});
   return (
     <>
       <div className="EditForm">
         <h1>Edit Connection</h1>
       </div>
       <div>
-        <Link to="/">
+        <div className="d-flex justify-content-center">
+          <Form className="edit-form-wrapper" onSubmit={(e) => saveConfig(e)}>
+            <Form.Group className="nickname" controlId="connectionNickname">
+              <Form.Label className="form-label">Nickname</Form.Label>
+              <Form.Control
+                className="form-field"
+                value={form.nickname}
+                onChange={(e) => {
+                  updateField('nickname', e.target.value);
+                }}
+                type="text"
+                placeholder="Enter nickname"
+              />
+            </Form.Group>
+          </Form>
+        </div>
+        <Link to="/RecentConnections">
           <Button className="">Return</Button>
         </Link>
       </div>
