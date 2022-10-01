c78983a81385dd7886b4f716d53e36b60f1c11f4
smulloy3 <smulloy3@gatech.edu>
2022-10-01 16:22:23 -0400

    Reuse new connection form

--- src/renderer/components/EditForm.tsx
+++ src/renderer/components/EditForm.tsx
@@ -1,62 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import DBProvider from '../../db/entity/enum';
+import { Button } from 'react-bootstrap';
 import { ConnectionModel } from '../../db/Models';
 import '../scss/RecentConnections.scss';
+import NewConnectionForm from './NewConnectionForm';
 
-interface INewConnectionForm {
-  id: number;
-  nickname: string;
-  type: DBProvider.PostgreSQL;
-  connectionConfig: {
-    config: 'manual';
-    address: string;
-    port: number;
-    username: string;
-    password: string;
-  };
-  createdDate: undefined;
-  lastUsed: undefined;
+interface IEditProps {
+  config: ConnectionModel;
 }
 
-const EditForm = () => {
-  const [form, setForm] = useState<ConnectionModel>(); // Todo: fix this
-  const updateField = (field: string, value: string) => {
-    setForm({
-      ...form,
-      [field]: value,
-    });
-  };
-  const saveConfig = async (event: React.FormEvent<HTMLElement>) => {
-    event.preventDefault();
-  };
+const EditForm = ({ config }: IEditProps) => {
+  const form = useState<ConnectionModel>(config);
   useEffect(() => {});
   return (
     <>
       <div className="EditForm">
         <h1>Edit Connection</h1>
       </div>
-      <div>
-        <div className="d-flex justify-content-center">
-          <Form className="edit-form-wrapper" onSubmit={(e) => saveConfig(e)}>
-            <Form.Group className="nickname" controlId="connectionNickname">
-              <Form.Label className="form-label">Nickname</Form.Label>
-              <Form.Control
-                className="form-field"
-                value={form.nickname}
-                onChange={(e) => {
-                  updateField('nickname', e.target.value);
-                }}
-                type="text"
-                placeholder="Enter nickname"
-              />
-            </Form.Group>
-          </Form>
-        </div>
-        <Link to="/RecentConnections">
-          <Button className="">Return</Button>
-        </Link>
+      {React.cloneElement(<NewConnectionForm />, {
+        defaultForm: { ...form },
+      })}
+      <div className="home-btn-footer">
+        <Button className="home-btn">Home</Button>
       </div>
     </>
   );
