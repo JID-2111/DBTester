9eeddbbebe130b8a29c975d8f55a5493e355b157
Aditya Vidyadharan <aditya@vidyadharan.com>
2022-09-30 22:41:11 -0400

    Trigger select connection on click

--- src/renderer/components/RecentConnections.tsx
+++ src/renderer/components/RecentConnections.tsx
@@ -14,13 +14,12 @@ const RecentConnections = () => {
     getConnections();
   }, []);
   const handledelete = async (ConnectionID: number) => {
-    const index = connect.findIndex(
-      (connection) => connection.id === ConnectionID
-    );
-    window.connections.ipcRenderer.delete(ConnectionID);
-    const newList = [...connect];
-    newList.splice(index, 1);
-    setConnect(newList);
+    await window.connections.ipcRenderer.delete(ConnectionID);
+    const connections = await window.connections.ipcRenderer.fetch();
+    setConnect(connections);
+  };
+  const handleSelect = async (ConnectionID: number) => {
+    await window.connections.ipcRenderer.select(ConnectionID);
   };
   while (connect.length > 5) {
     connect.shift();
@@ -47,7 +46,11 @@ const RecentConnections = () => {
                   <tr key={value.id}>
                     <td className="LinkTD">
                       <Link to="/Execute">
-                        <button className="buttonSelect" type="button">
+                        <button
+                          className="buttonSelect"
+                          type="button"
+                          onClick={() => handleSelect(value.id)}
+                        >
                           {value.nickname}
                         </button>
                       </Link>
