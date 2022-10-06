32896ff980a600c7ec4a9dea78e02c3b9a1760ca
Aditya Vidyadharan <aditya@vidyadharan.com>
2022-10-06 00:48:13 -0400

    Remove test code

--- src/main/main.ts
+++ src/main/main.ts
@@ -129,7 +129,6 @@ app
   .whenReady()
   .then(async () => {
     await AppDataSource.initialize();
-    new ConnectionService().test();
     createWindow();
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
