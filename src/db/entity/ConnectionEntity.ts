9ae9100b424c1482f5b5064460a285280331f90f
Aditya Vidyadharan <aditya@vidyadharan.com>
2022-10-14 00:09:58 -0400

    remove post remove cleanup code

--- src/db/entity/ConnectionEntity.ts
+++ src/db/entity/ConnectionEntity.ts
@@ -1,5 +1,4 @@
 import { safeStorage } from 'electron';
-import log from 'electron-log';
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -9,9 +8,7 @@ import {
   BeforeInsert,
   BeforeUpdate,
   OneToMany,
-  AfterRemove,
 } from 'typeorm';
-import AppDataSource from '../../data-source';
 import { parseConnectionString } from '../../main/util';
 import {
   ConnectionInputType,
@@ -60,30 +57,6 @@ class ConnectionEntity {
    */
   @OneToMany((_type) => ExecutionEntity, (execution) => execution.connections)
   executions: ExecutionEntity[];
-
-  /**
-   * If a connection is removed, delete any executions associated with it
-   */
-  @AfterRemove()
-  async cleanupExecutions() {
-    console.log('triggered');
-    const rep = AppDataSource.getRepository(ExecutionEntity);
-    const orphans = await rep
-      .createQueryBuilder()
-      .leftJoinAndSelect(
-        'execution_connections',
-        'ec',
-        'ExecutionEntity.id=ec.execution'
-      )
-      .where('ec.connection is null')
-      .getMany();
-    log.debug(
-      `Removing orphan executions with id: [${orphans.map(
-        (entity) => entity.id
-      )}]`
-    );
-    await rep.remove(orphans);
-  }
 
   @AfterLoad()
   decryptPassword() {
