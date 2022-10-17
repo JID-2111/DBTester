2d895c4cb848f5dda3527e6a02f151a00edb59b6
SeanMulloy <cattlauer82@gmail.com>
2022-10-16 20:48:07 -0400

    Add 'Select Table' option

---
+++ src/renderer/components/execute/TableDropdown.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+import '../../scss/Execute.scss';
+
+interface ITableDropdownProps {
+  activeDb: string;
+}
+
+const TableDropdown = ({ activeDb }: ITableDropdownProps) => {
+  const tablesDefault = ['test1', 'test2', 'test3'];
+  const [tables, setTables] = useState<string[]>();
+  const [activeTable, setActiveTable] = useState<string>('Select Table');
+  useEffect(() => {
+    const fetchTables = async () => {
+      const newTables = await window.procedures.ipcRenderer.fetchTables();
+      setTables(newTables);
+    };
+    fetchTables();
+  }, [activeDb]);
+  const handleClick = async (table: string) => {
+    setActiveTable(table);
+  };
+  const showTables = () => {
+    return (tables || tablesDefault).map((table) => {
+      return (
+        <Dropdown.Item key={table} onClick={() => handleClick(table)}>
+          {table}
+        </Dropdown.Item>
+      );
+    });
+  };
+  return (
+    <>
+      <Dropdown className="dropdown">
+        <Dropdown.Toggle variant="primary">{activeTable}</Dropdown.Toggle>
+        <Dropdown.Menu>{showTables()}</Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
+};
+
+export default TableDropdown;
