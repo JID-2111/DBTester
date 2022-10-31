78ed7e97d98b4aa1fa154e3044a4b97ff5724278
Ruben Agredano <Ruben@Mac-mini.local>
2022-10-31 12:27:56 -0400

    need styling

--- src/renderer/components/execute/Results.tsx
+++ src/renderer/components/execute/Results.tsx
@@ -1,10 +1,12 @@
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, Accordion } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import { ExecutionModelType } from 'db/models/ExecutionModel';
 import { RuleModelType } from 'db/models/RuleModel';
-import AllPass from './AllPass';
-import Displaytests from './Displaytests';
+import { CheckLg, XLg } from 'react-bootstrap-icons';
 import '../../scss/results.scss';
+import AccordionItem from 'react-bootstrap/esm/AccordionItem';
+import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
+import AccordionBody from 'react-bootstrap/esm/AccordionBody';
 
 interface ILocationState {
   results: ExecutionModelType;
@@ -30,35 +32,46 @@ const Results = () => {
         <div>
           <h1>Execution Results</h1>
         </div>
-        <Table className="table">
-          <thead>
-            <tr key={results.id}>
-              <th>Name</th>
-              <th>ID</th>
-              <th>Database</th>
-              <th>Type</th>
-              <th>Result</th>
-              <th> Message </th>
-            </tr>
-          </thead>
-          <tbody>
-            {results.rules.map((value) => {
-              return (
-                <>
-                  {passFail(value) ? (
-                    <AllPass Rule={value} />
+        <Accordion className="accordionClass">
+          {results.rules.map((rule) => {
+            return (
+              <AccordionItem eventKey="0">
+                <AccordionHeader>
+                  {passFail(rule) ? (
+                    <>{(rule.name, (<CheckLg />))}</>
                   ) : (
-                    <Displaytests Rule={value} />
+                    rule.name
                   )}
-                </>
-              );
-            })}
-          </tbody>
-        </Table>
+                </AccordionHeader>
+                <AccordionBody>
+                  {rule.unitTests.map((Test) => {
+                    return (
+                      <Table className="Results-Table">
+                        <tr key={Test.id}>
+                          <td>{Test.name}</td>
+                          {Test.result ? (
+                            <td> test Passed </td>
+                          ) : (
+                            <td> Test Failed</td>
+                          )}
+                          {Test.result ? <CheckLg /> : <XLg />}
+                          <td> {Test.output}</td>
+                        </tr>
+                      </Table>
+                    );
+                  })}
+                </AccordionBody>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
       </div>
       <div className="homeButton">
         <Link to="/">
           <Button type="button">Home</Button>
+        </Link>
+        <Link to="/Execute">
+          <Button>Back</Button>
         </Link>
       </div>
     </div>
