330e1415bf42daf5deacb02f2ae39e569bfa8c11
Ruben Agredano <Ruben@Mac-mini.local>
2022-10-31 19:02:36 -0400

    milestone

--- src/renderer/components/execute/Results.tsx
+++ src/renderer/components/execute/Results.tsx
@@ -7,6 +7,14 @@ import '../../scss/results.scss';
 import AccordionItem from 'react-bootstrap/esm/AccordionItem';
 import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
 import AccordionBody from 'react-bootstrap/esm/AccordionBody';
+import {
+  TableGenericOperations,
+  UnitTestOperations,
+  OutputFormat,
+  RecordMatches,
+  RowNumberOperations,
+} from 'db/entity/enum';
+import { TableTestType, RowTestType } from 'db/models/UnitTestModels';
 
 interface ILocationState {
   results: ExecutionModelType;
@@ -15,7 +23,109 @@ const Results = () => {
   const location = useLocation();
   const state = location.state as ILocationState;
   const { results } = state;
+  const execution: ExecutionModelType = {
+    timestamp: new Date(),
+    rules: [],
+  };
 
+  const rule1: RuleModelType = {
+    name: 'rule1',
+    ruleId: 0,
+    database: 'React',
+    testData: '',
+    unitTests: [],
+    execution,
+    procedure: '',
+    parameters: [],
+  };
+
+  const rule2: RuleModelType = {
+    name: 'rule2',
+    ruleId: 1,
+    database: 'React',
+    testData: '',
+    unitTests: [],
+    execution,
+    procedure: '',
+    parameters: [],
+  };
+  const test1: TableTestType = {
+    operation: TableGenericOperations.EXISTS,
+    level: UnitTestOperations.TableGenericOperations,
+    name: 'test1',
+    expectedRecordMatches: RecordMatches.TABLE_ROWS,
+    total: false,
+    expectedNumRecords: 0,
+    table: 'accounts',
+    result: true,
+    format: OutputFormat.PLAIN,
+    output: '',
+    rule: rule1,
+  };
+
+  const test2: TableTestType = {
+    operation: TableGenericOperations.COUNT,
+    level: UnitTestOperations.TableGenericOperations,
+    name: 'test2',
+    expectedRecordMatches: RecordMatches.TABLE_ROWS,
+    total: true,
+    expectedNumRecords: 10,
+    table: 'accounts',
+    result: false,
+    format: OutputFormat.PLAIN,
+    output: '',
+    rule: rule1,
+  };
+
+  const test3: TableTestType = {
+    operation: TableGenericOperations.COUNT,
+    level: UnitTestOperations.TableGenericOperations,
+    name: 'test3',
+    expectedRecordMatches: RecordMatches.TABLE_ROWS,
+    total: false,
+    expectedNumRecords: 2,
+    table: 'accounts',
+    result: false,
+    format: OutputFormat.PLAIN,
+    output: '',
+    rule: rule1,
+  };
+
+  const test4: RowTestType = {
+    operation: RowNumberOperations.LT,
+    level: UnitTestOperations.RowNumberOperations,
+    name: 'test4',
+    expectedRecordMatches: RecordMatches.TABLE_ROWS,
+    total: true,
+    column: 'balance',
+    expectedNumRecords: 2,
+    table: 'accounts',
+    result: true,
+    format: OutputFormat.PLAIN,
+    output: '',
+    value: '200',
+    rule: rule2,
+  };
+
+  const test5: RowTestType = {
+    operation: RowNumberOperations.EQ,
+    level: UnitTestOperations.RowNumberOperations,
+    name: 'test5',
+    expectedRecordMatches: RecordMatches.ZERO,
+    total: true,
+    column: 'balance',
+    expectedNumRecords: 2,
+    table: 'accounts',
+    result: true,
+    format: OutputFormat.PLAIN,
+    output: '',
+    value: '100',
+    rule: rule2,
+  };
+
+  rule1.unitTests = [test1, test2, test3];
+  rule2.unitTests = [test4, test5];
+  execution.rules = [rule1, rule2];
   const passFail = (Rule: RuleModelType): boolean => {
     let pass = true;
     Rule.unitTests.forEach((test) => {
@@ -32,16 +142,12 @@ const Results = () => {
         <div>
           <h1>Execution Results</h1>
         </div>
-        <Accordion className="accordionClass">
-          {results.rules.map((rule) => {
-            return (
+        {execution.rules.map((rule) => {
+          return (
+            <Accordion className="accordionClass">
               <AccordionItem eventKey="0">
                 <AccordionHeader>
-                  {passFail(rule) ? (
-                    <>{(rule.name, (<CheckLg />))}</>
-                  ) : (
-                    rule.name
-                  )}
+                  {passFail(rule) ? (rule.name, (<CheckLg />)) : rule.name}
                 </AccordionHeader>
                 <AccordionBody>
                   {rule.unitTests.map((Test) => {
@@ -62,9 +168,9 @@ const Results = () => {
                   })}
                 </AccordionBody>
               </AccordionItem>
-            );
-          })}
-        </Accordion>
+            </Accordion>
+          );
+        })}
       </div>
       <div className="homeButton">
         <Link to="/">
