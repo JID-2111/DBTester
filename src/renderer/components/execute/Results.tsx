d9780bf73f935e7e9dfce9b24fa88086b0daeb5a
Aditya Vidyadharan <aditya@vidyadharan.com>
2022-11-12 16:57:16 -0500

    Styling changes and remove hard coded values

--- src/renderer/components/execute/Results.tsx
+++ src/renderer/components/execute/Results.tsx
@@ -2,19 +2,16 @@ import { Table, Button, Accordion } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import { ExecutionModelType } from 'db/models/ExecutionModel';
 import { RuleModelType } from 'db/models/RuleModel';
-import { CheckLg, XLg } from 'react-bootstrap-icons';
+import {
+  CheckCircle,
+  CheckCircleFill,
+  XCircle,
+  XCircleFill,
+} from 'react-bootstrap-icons';
 import '../../scss/results.scss';
 import AccordionItem from 'react-bootstrap/esm/AccordionItem';
 import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
 import AccordionBody from 'react-bootstrap/esm/AccordionBody';
-import {
-  TableGenericOperations,
-  UnitTestOperations,
-  OutputFormat,
-  RecordMatches,
-  RowNumberOperations,
-} from 'db/entity/enum';
-import { TableTestType, RowTestType } from 'db/models/UnitTestModels';
 
 interface ILocationState {
   results: ExecutionModelType;
@@ -25,109 +22,110 @@ const Results = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   // disable for now until execute is finished
   const { results } = state;
-  const execution: ExecutionModelType = {
-    timestamp: new Date(),
-    rules: [],
-  };
+  // const execution: ExecutionModelType = {
+  //   timestamp: new Date(),
+  //   rules: [],
+  // };
 
-  const rule1: RuleModelType = {
-    name: 'rule1',
-    ruleId: 0,
-    database: 'React',
-    testData: '',
-    unitTests: [],
-    execution,
-    procedure: '',
-    parameters: [],
-  };
+  // const rule1: RuleModelType = {
+  //   name: 'rule1',
+  //   ruleId: 0,
+  //   database: 'React',
+  //   testData: '',
+  //   unitTests: [],
+  //   execution,
+  //   procedure: '',
+  //   parameters: [],
+  // };
 
-  const rule2: RuleModelType = {
-    name: 'rule2',
-    ruleId: 1,
-    database: 'React',
-    testData: '',
-    unitTests: [],
-    execution,
-    procedure: '',
-    parameters: [],
-  };
-  const test1: TableTestType = {
-    operation: TableGenericOperations.EXISTS,
-    level: UnitTestOperations.TableGenericOperations,
-    name: 'test1',
-    expectedRecordMatches: RecordMatches.TABLE_ROWS,
-    total: false,
-    expectedNumRecords: 0,
-    table: 'accounts',
-    result: true,
-    format: OutputFormat.PLAIN,
-    output: 'good job',
-    rule: rule1,
-  };
+  // const rule2: RuleModelType = {
+  //   name: 'rule2',
+  //   ruleId: 1,
+  //   database: 'React',
+  //   testData: '',
+  //   unitTests: [],
+  //   execution,
+  //   procedure: '',
+  //   parameters: [],
+  // };
+  // const test1: TableTestType = {
+  //   operation: TableGenericOperations.EXISTS,
+  //   level: UnitTestOperations.TableGenericOperations,
+  //   name: 'test1',
+  //   expectedRecordMatches: RecordMatches.TABLE_ROWS,
+  //   total: false,
+  //   expectedNumRecords: 0,
+  //   table: 'accounts',
+  //   result: true,
+  //   format: OutputFormat.PLAIN,
+  //   output: 'good job',
+  //   rule: rule1,
+  // };
 
-  const test2: TableTestType = {
-    operation: TableGenericOperations.COUNT,
-    level: UnitTestOperations.TableGenericOperations,
-    name: 'test2',
-    expectedRecordMatches: RecordMatches.TABLE_ROWS,
-    total: true,
-    expectedNumRecords: 10,
-    table: 'accounts',
-    result: false,
-    format: OutputFormat.PLAIN,
-    output: 'this test did not pass',
-    rule: rule1,
-  };
+  // const test2: TableTestType = {
+  //   operation: TableGenericOperations.COUNT,
+  //   level: UnitTestOperations.TableGenericOperations,
+  //   name: 'test2',
+  //   expectedRecordMatches: RecordMatches.TABLE_ROWS,
+  //   total: true,
+  //   expectedNumRecords: 10,
+  //   table: 'accounts',
+  //   result: false,
+  //   format: OutputFormat.PLAIN,
+  //   output: 'this test did not pass',
+  //   rule: rule1,
+  // };
 
-  const test3: TableTestType = {
-    operation: TableGenericOperations.COUNT,
-    level: UnitTestOperations.TableGenericOperations,
-    name: 'test3',
-    expectedRecordMatches: RecordMatches.TABLE_ROWS,
-    total: false,
-    expectedNumRecords: 2,
-    table: 'accounts',
-    result: false,
-    format: OutputFormat.PLAIN,
-    output: 'test could use some work',
-    rule: rule1,
-  };
+  // const test3: TableTestType = {
+  //   operation: TableGenericOperations.COUNT,
+  //   level: UnitTestOperations.TableGenericOperations,
+  //   name: 'test3',
+  //   expectedRecordMatches: RecordMatches.TABLE_ROWS,
+  //   total: false,
+  //   expectedNumRecords: 2,
+  //   table: 'accounts',
+  //   result: false,
+  //   format: OutputFormat.PLAIN,
+  //   output: 'test could use some work',
+  //   rule: rule1,
+  // };
 
-  const test4: RowTestType = {
-    operation: RowNumberOperations.LT,
-    level: UnitTestOperations.RowNumberOperations,
-    name: 'test4',
-    expectedRecordMatches: RecordMatches.TABLE_ROWS,
-    total: true,
-    column: 'balance',
-    expectedNumRecords: 2,
-    table: 'accounts',
-    result: true,
-    format: OutputFormat.PLAIN,
-    output: 'output goes here',
-    value: '200',
-    rule: rule2,
-  };
+  // const test4: RowTestType = {
+  //   operation: RowNumberOperations.LT,
+  //   level: UnitTestOperations.RowNumberOperations,
+  //   name: 'test4',
+  //   expectedRecordMatches: RecordMatches.TABLE_ROWS,
+  //   total: true,
+  //   column: 'balance',
+  //   expectedNumRecords: 2,
+  //   table: 'accounts',
+  //   result: true,
+  //   format: OutputFormat.PLAIN,
+  //   output: 'output goes here',
+  //   value: '200',
+  //   rule: rule2,
+  // };
 
-  const test5: RowTestType = {
-    operation: RowNumberOperations.EQ,
-    level: UnitTestOperations.RowNumberOperations,
-    name: 'test5',
-    expectedRecordMatches: RecordMatches.ZERO,
-    total: true,
-    column: 'balance',
-    expectedNumRecords: 2,
-    table: 'accounts',
-    result: true,
-    format: OutputFormat.PLAIN,
-    output: 'this is an output',
-    value: '100',
-    rule: rule2,
-  };
+  // const test5: RowTestType = {
+  //   operation: RowNumberOperations.EQ,
+  //   level: UnitTestOperations.RowNumberOperations,
+  //   name: 'test5',
+  //   expectedRecordMatches: RecordMatches.ZERO,
+  //   total: true,
+  //   column: 'balance',
+  //   expectedNumRecords: 2,
+  //   table: 'accounts',
+  //   result: true,
+  //   format: OutputFormat.PLAIN,
+  //   output: 'this is an output',
+  //   value: '100',
+  //   rule: rule2,
+  // };
 
-  rule1.unitTests = [test1, test2, test3];
-  rule2.unitTests = [test4, test5];
-  execution.rules = [rule1, rule2];
+  // rule1.unitTests = [test1, test2, test3];
+  // rule2.unitTests = [test4, test5];
+  // execution.rules = [rule1, rule2];
+
   const passFail = (Rule: RuleModelType): boolean => {
     let pass = true;
     Rule.unitTests.forEach((test) => {
@@ -144,18 +142,19 @@ const Results = () => {
         <div>
           <h1>Execution Results</h1>
         </div>
-        {execution.rules.map((rule) => {
+        {results.rules.map((rule) => {
           return (
             <Accordion className="accordionClass">
               <AccordionItem eventKey="0">
                 <AccordionHeader id="header">
-                  {passFail(rule) ? (
-                    <>
-                      {rule.name} <CheckLg className="Check" />
-                    </>
-                  ) : (
-                    rule.name
-                  )}
+                  <>
+                    {passFail(rule) ? (
+                      <CheckCircleFill className="Check" color="green" />
+                    ) : (
+                      <XCircleFill className="X" color="red" />
+                    )}
+                    {rule.name}
+                  </>
                 </AccordionHeader>
                 <AccordionBody>
                   <Table className="Results-Table">
@@ -171,11 +170,11 @@ const Results = () => {
                             )}
                             {Test.result ? (
                               <td>
-                                <CheckLg className="Check" />
+                                <CheckCircle className="Check" />
                               </td>
                             ) : (
                               <td>
-                                <XLg className="X" />
+                                <XCircle className="X" />
                               </td>
                             )}
                             <td> {Test.output}</td>
