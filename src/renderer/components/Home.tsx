c2993fd8f18e5d9a0abed045f5d3cafb52473697
alex-hobby <alexhobby98@gmail.com>
2022-11-20 01:13:34 -0500

    match home sidebar to execute, clean up some scss

--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, Col, Container } from 'react-bootstrap';
 import { Archive, BarChartFill, Link45deg, Plus } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 import RecentList from './recent_connections/Recent';
@@ -7,35 +7,33 @@ import '../scss/Home.scss';
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="menu-wrapper">
-        <div className="recent-wrapper">
-          <RecentList />
-        </div>
-        <div className="launch-wrapper">
-          <BarChartFill className="logo" height="5em" width="5em" />
-          <h1>Welcome to DB Tester</h1>
-          <Link to="/newconnection">
-            <Button variant="link" className="launch-button">
-              <Plus className="icon" height="2em" width="2em" />
-              <h4>New Connection</h4>
-            </Button>
-          </Link>
-          <Link to="/RecentConnection">
-            <Button variant="link" className="launch-button">
-              <Link45deg className="icon" height="2em" width="2em" />
-              <h4>Connect to Existing</h4>
-            </Button>
-          </Link>
-          <Link to="/History">
-            <Button variant="link" className="launch-button">
-              <Archive className="icon" height="2em" width="2em" />
-              <h4>View History</h4>
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <Container fluid className="home-wrapper">
+      <Col className="sidebar">
+        <RecentList />
+      </Col>
+      <Col className="launch-wrapper">
+        <BarChartFill className="logo" height="5em" width="5em" />
+        <h1>Welcome to DB Tester</h1>
+        <Link to="/newconnection">
+          <Button variant="link" className="launch-button">
+            <Plus className="icon" height="2em" width="2em" />
+            <h4>New Connection</h4>
+          </Button>
+        </Link>
+        <Link to="/RecentConnection">
+          <Button variant="link" className="launch-button">
+            <Link45deg className="icon" height="2em" width="2em" />
+            <h4>Connect to Existing</h4>
+          </Button>
+        </Link>
+        <Link to="/History">
+          <Button variant="link" className="launch-button">
+            <Archive className="icon" height="2em" width="2em" />
+            <h4>View History</h4>
+          </Button>
+        </Link>
+      </Col>
+    </Container>
   );
 };
 
